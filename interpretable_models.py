958334ebd0059424646280d5194bf3c3bd54a804
Aakash raj <82706959+easyans@users.noreply.github.com>
2024-02-16 23:04:08 +0530

    Update interpretable_models.py

--- interpretable_models.py
+++ interpretable_models.py
@@ -1,68 +1,52 @@
-
 # %% Imports
 from utils import DataLoader
-from interpret.glassbox import (LogisticRegression,
-                                ClassificationTree, 
-                                ExplainableBoostingClassifier)
-from interpret import show
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score, accuracy_score
+import shap
 
 # %% Load and preprocess data
 data_loader = DataLoader()
 data_loader.load_dataset()
 data_loader.preprocess_data()
-
 # Split the data for evaluation
 X_train, X_test, y_train, y_test = data_loader.get_data_split()
-print(X_train.shape)
-print(X_test.shape)
 # Oversample the train data
 X_train, y_train = data_loader.oversample(X_train, y_train)
-print("After oversampling:", X_train.shape)
+print(X_train.shape)
+print(X_test.shape)
 
-# %% Fit logistic regression model
-lr = LogisticRegression(random_state=2021, feature_names=X_train.columns, penalty='l1', solver='liblinear')
-lr.fit(X_train, y_train)
-print("Training finished.")
-
-# %% Evaluate logistic regression model
-y_pred = lr.predict(X_test)
+# %% Fit blackbox model
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
 print(f"F1 Score {f1_score(y_test, y_pred, average='macro')}")
 print(f"Accuracy {accuracy_score(y_test, y_pred)}")
 
-# %% Explain local prediction
-lr_local = lr.explain_local(X_test[:100], y_test[:100], name='Logistic Regression')
-show(lr_local)
+# %% Create SHAP explainer
+explainer = shap.TreeExplainer(rf)
+# Calculate shapley values for test data
+start_index = 1
+end_index = 2
+shap_values = explainer.shap_values(X_test[start_index:end_index])
+X_test[start_index:end_index]
 
-# %% Explain global logistic regression model
-lr_global = lr.explain_global(name='Logistic Regression')
-show(lr_global)
+# %% Investigating the values (classification problem)
+# class 0 = contribution to class 1
+# class 1 = contribution to class 2
+print(shap_values[0].shape)
+shap_values
 
-# %% Fit decision tree model
-tree = ClassificationTree()
-tree.fit(X_train, y_train)
-print("Training finished.")
-y_pred = tree.predict(X_test)
-print(f"F1 Score {f1_score(y_test, y_pred, average='macro')}")
-print(f"Accuracy {accuracy_score(y_test, y_pred)}")
+# %% >> Visualize local predictions
+shap.initjs()
+# Force plot
+prediction = rf.predict(X_test[start_index:end_index])[0]
+print(f"The RF predicted: {prediction}")
+shap.force_plot(explainer.expected_value[1],
+                shap_values[1],
+                X_test[start_index:end_index], # for values
+)
 
-# %% Explain local prediction
-tree_local = tree.explain_local(X_test[:100], y_test[:100], name='Tree')
-show(tree_local)
-
-# %% Fit Explainable Boosting Machine
-ebm = ExplainableBoostingClassifier(random_state=2021)
-ebm.fit(X_train, y_train)
-print("Training finished.")
-y_pred = ebm.predict(X_test)
-print(f"F1 Score {f1_score(y_test, y_pred, average='macro')}")
-print(f"Accuracy {accuracy_score(y_test, y_pred)}")
-
-# %% Explain locally
-ebm_local = ebm.explain_local(X_test[:100], y_test[:100], name='EBM')
-show(ebm_local)
-
-# %% Explain globally
-ebm_global = ebm.explain_global(name='EBM')
-show(ebm_global)
+# %% >> Visualize global features
+# Feature summary
+shap.summary_plot(shap_values, X_test)
 # %%
